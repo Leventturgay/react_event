22e607139cd2815d8a05bded2c6f0e4bab21db39
Turgay Levent <turgayleventt@gmail.com>
2022-10-21 11:31:15 +0300

    events

--- src/components/1-events/events.jsx
+++ src/components/1-events/events.jsx
@@ -1,8 +1,20 @@
 
 const events = () => {
+const handleClick=()=>{
+  alert("Buton Clicked")
+};
+const handleClear=(msg)=>{
+alert(msg);
+};
+const handleChange=(event)=>{
+  console.log(event.target)
+};
+
   return (
-    <div>
-      <button>Click</button>
+    <div className="container text-center mt-4">
+      <button onClick={handleClick} className="btn btn-success">Click</button>
+      <button onClick={()=>handleClear("Clear btn Clicked")} className="btn btn-dark">Clear</button>
+      <button onClick={handleChange} className="btn btn-danger">Change</button>
     </div>
   )
 }
