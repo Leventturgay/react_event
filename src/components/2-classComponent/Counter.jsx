c98a329bcd68bb8e383e81d6380d40d31f5e0d94
Turgay Levent <turgayleventt@gmail.com>
2022-10-21 12:47:57 +0300

    counter

--- src/components/2-classComponent/Counter.jsx
+++ src/components/2-classComponent/Counter.jsx
@@ -5,14 +5,32 @@ import  { Component } from 'react'
     super(props)
   
     this.state = {
-       count:0,
+       count:props.count|| 0,
     }
+    // Binding metodu
+    this.increment = this.increment.bind(this);
   }
+  increment(){
+    this.setState({
+      count : this.state.count + 1
+    });
+  }
+  decrement = () => {
+    this.setState({
+      count : this.state.count - 1,
+    });
+  };
+
   render() {
     return (
-      <div>Counter</div>
+      <div className="container text-center mt-4">
+      <h1> ClassFull Component </h1>
+      <h2 className='display-4 text-danger'>Count:{this.state.count} </h2>
+      <button onClick={this.increment} className="btn btn-success">Increment</button>
+      <button onClick={this.decrement} className="btn btn-danger">Decrement</button>
+    </div>
     )
   }
 }
 
-export default Counter
+export default Counter;
