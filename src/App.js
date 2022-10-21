1dbb3b8ab11d53f6421cb744e654d3a8dffb7e48
Turgay Levent <turgayleventt@gmail.com>
2022-10-21 12:19:12 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Events from "./components/1-events/events";
-
+import Counter from "./components/2-classComponent/Counter";
 function App() {
   return (
     <div>
       <Events />
+      <Counter />
     </div>
   );
 }
--- src/components/1-events/events.jsx
+++ src/components/1-events/events.jsx
@@ -1,5 +1,6 @@
 
 const events = () => {
+  let message= "Event Based Message"
 const handleClick=()=>{
   alert("Buton Clicked")
 };
@@ -7,11 +8,13 @@ const handleClear=(msg)=>{
 alert(msg);
 };
 const handleChange=(event)=>{
+  message= "react"
   console.log(event.target)
 };
 
   return (
     <div className="container text-center mt-4">
+      <h1> {message} </h1>
       <button onClick={handleClick} className="btn btn-success">Click</button>
       <button onClick={()=>handleClear("Clear btn Clicked")} className="btn btn-dark">Clear</button>
       <button onClick={handleChange} className="btn btn-danger">Change</button>
