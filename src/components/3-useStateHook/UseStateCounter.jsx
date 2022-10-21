9ca7104f82203be4dbefbf0db17086a8af2ac05a
Turgay Levent <turgayleventt@gmail.com>
2022-10-21 15:40:38 +0300

    event_hook

--- src/components/3-useStateHook/UseStateCounter.jsx
+++ src/components/3-useStateHook/UseStateCounter.jsx
@@ -2,26 +2,47 @@ import { useState } from "react";
 
 const UseStateCounter = () => {
   const [count, setCount] = useState(0);
+  const [person, setPerson] = useState({
+    name: "Can",
+    surname: "Doe",
+    age: 43,
+  });
   const increment = () => {
     setCount(count + 1);
   };
   const decrement = () => {
-    setCount(count - 1);
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  const incAge = () => {
+    setPerson({ ...person, age: person.age + 1 });
   };
   return (
     <div className="container text-center mt-4">
-      <h1> Use State Hook </h1>
-      <h2 className="display-4 text-danger">Count:{count} </h2>
-      <button onClick={increment} className="btn btn-success">
-        Increment
-      </button>
-      <button onClick={() => setCount(0)} className="btn btn-dark">
-        CLR
-      </button>
+      <section>
+        <h1> Use State Hook </h1>
+        <h2 className="display-4 text-danger">Count:{count} </h2>
+        <button onClick={increment} className="btn btn-success">
+          Increment
+        </button>
+        <button onClick={() => setCount(0)} className="btn btn-dark">
+          CLR
+        </button>
 
-      <button onClick={decrement} className="btn btn-danger">
-        Decrement
-      </button>
+        <button onClick={decrement} className="btn btn-danger">
+          Decrement
+        </button>
+      </section>
+      <section>
+        <h1>UseState Object</h1>
+        <h2> {person.name} </h2>
+        <h2> {person.surname} </h2>
+        <h2> {person.age} </h2>
+        <button onClick={incAge} className="btn btn-info">
+          ins age
+        </button>
+      </section>
     </div>
   );
 };
